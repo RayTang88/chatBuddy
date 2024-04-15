c42e610d7d3f35833c0f5728932aa38116eff0b3
Raytang88 <tc251369779@163.com>
2024-04-15 20:36:26 +0800

    change requiment.txt

--- app.py
+++ app.py
@@ -1,41 +1,27 @@
 import gradio as gr
 import os
-import torch
-# from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
+from lmdeploy import pipeline, TurbomindEngineConfig
 from openxlab.model import download
 
-base_path = './internlm2-1.8b-4bit-awq'
+base_path = './internlm2-chat-1_8b-4bit-awq'
 os.system(f'git clone https://code.openxlab.org.cn/Raytang88/internlm2-1.8b-4bit-awq.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
-# tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
-# model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
-
-# def chat(message,history):
-#     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
-#         yield response
-
-# gr.ChatInterface(chat,
-#                  title="internlm2-1.8b-4bit-awq",
-#                 description="""
-# InternLM is mainly developed by Shanghai AI Laboratory.  
-#                  """,
-#                  ).queue(1).launch()
-
-# import gradio as gr
-from lmdeploy import pipeline, TurbomindEngineConfig
-
-
 backend_config = TurbomindEngineConfig(session_len=8192, model_fomat='awq') # 图片分辨率较高时请调高session_len
-# pipe = pipeline('liuhaotian/llava-v1.6-vicuna-7b', backend_config=backend_config) 非开发机运行此命令
+
 pipe = pipeline(base_path, backend_config=backend_config)
 
-def model(message, history):
-    if history is None:
-        return [(message, "请说出你的问题。")]
-    else:
-        response = pipe((message, history)).text
-        return [(message, response)]
 
+def model(text):
+    if text is None:
+        return [(text, "请输入你的问题。")]
+    else:
+        response = pipe((text)).text
+    return [(text, response)]
+
+# demo = gr.Interface(fn=model, inputs=[gr.Image(type="pil"), gr.Textbox()], outputs=gr.Chatbot())
 demo = gr.Interface(fn=model, inputs=[gr.Textbox()], outputs=gr.Chatbot())
-demo.launch()  
+demo.launch()
+
+
+    
