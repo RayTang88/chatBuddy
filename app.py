c3cd41254889172e4f22057202c0d63a876d82b3
Raytang88 <tc251369779@163.com>
2024-04-16 13:45:48 +0800

    init push

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import gradio as gr
+import torch
 import os
 from lmdeploy import pipeline, TurbomindEngineConfig
-from openxlab.model import download
+# from openxlab.model import download
 
 base_path = './internlm2-chat-1_8b-4bit-awq'
-os.system(f'git clone https://code.openxlab.org.cn/Raytang88/internlm2-1.8b-4bit-awq.git {base_path}')
+os.system('apt install git')
+os.system('apt install git-lfs')
+os.system(f'git clone https://code.openxlab.org.cn/raytang88/internlm2-chat-1_8b-4bit-awq.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 backend_config = TurbomindEngineConfig(session_len=8192, model_fomat='awq') # 图片分辨率较高时请调高session_len
